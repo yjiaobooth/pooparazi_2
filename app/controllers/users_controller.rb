f69317cd7d3b7b686f85af22ef3f64359177a906
Raghu Betina <raghu@firstdraft.com>
2018-03-11 02:30:04 +0000

    Generate users resource with starter:resource

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,87 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+
+    render("users/index.html.erb")
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    render("users/show.html.erb")
+  end
+
+  def new
+    @user = User.new
+
+    render("users/new.html.erb")
+  end
+
+  def create
+    @user = User.new
+
+    @user.email = params[:email]
+    @user.username = params[:username]
+    @user.first_name = params[:first_name]
+    @user.last_name = params[:last_name]
+    @user.password = params[:password]
+
+    save_status = @user.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/users/new", "/create_user"
+        redirect_to("/users")
+      else
+        redirect_back(:fallback_location => "/", :notice => "User created successfully.")
+      end
+    else
+      render("users/new.html.erb")
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+
+    render("users/edit.html.erb")
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    @user.email = params[:email]
+    @user.username = params[:username]
+    @user.first_name = params[:first_name]
+    @user.last_name = params[:last_name]
+    @user.password = params[:password]
+
+    save_status = @user.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/users/#{@user.id}/edit", "/update_user"
+        redirect_to("/users/#{@user.id}", :notice => "User updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "User updated successfully.")
+      end
+    else
+      render("users/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    @user.destroy
+
+    if URI(request.referer).path == "/users/#{@user.id}"
+      redirect_to("/", :notice => "User deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "User deleted.")
+    end
+  end
+end
