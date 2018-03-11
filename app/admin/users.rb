8830dd25b2c94babad4a4d51c9eeac099c3d02b8
Raghu Betina <raghu@firstdraft.com>
2018-03-11 02:30:40 +0000

    Whitelist columns in User for ActiveAdmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register User do
+
+ permit_params :email, :username, :first_name, :last_name, :password
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
